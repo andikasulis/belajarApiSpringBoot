de30fd4847afe6981f5e85408d24b0f96faa3884
Andika Sulis <andikasulispratama@yahoo.co.id>
2021-05-07 16:24:19 +0700

    Add Rest API Update Product

--- src/main/java/com/domain/controllers/ProductController.java
+++ src/main/java/com/domain/controllers/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,5 +32,10 @@ public class ProductController {
     @GetMapping("/{id}")
     public Product findOne(@PathVariable("id") Long id){
         return productService.findOne(id);
+    }
+
+    @PutMapping
+    public Product update(@RequestBody Product product){
+        return productService.save(product);
     }
 }
