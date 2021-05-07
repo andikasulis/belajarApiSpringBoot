4bf8bcb74a09ac0fbd43a8596546372cc71dad30
Andika Sulis <andikasulispratama@yahoo.co.id>
2021-05-07 16:17:42 +0700

    Add Rest Api Products

---
+++ src/main/java/com/domain/controllers/WelcomeController.java
@@ -0,0 +1,23 @@
+package com.domain.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/welcome")
+public class WelcomeController{
+
+    @GetMapping
+    public String welcome(){
+        return "Welcome to spring boot api";
+    }
+
+    // @PostMapping
+    // public String other(){        
+    //     return "Data sesuatu";
+    // }
+    
+
+}
