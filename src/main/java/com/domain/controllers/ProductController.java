d7c27f68b4354312b58d76152e3abaa8f8db75d5
Andika Sulis <andikasulispratama@yahoo.co.id>
2021-05-08 10:55:01 +0700

    add api delete

--- src/main/java/com/domain/controllers/ProductController.java
+++ src/main/java/com/domain/controllers/ProductController.java
@@ -4,6 +4,7 @@ import com.domain.models.entities.Product;
 import com.domain.services.ProductService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,6 +33,11 @@ public class ProductController {
     @GetMapping("/{id}")
     public Product findOne(@PathVariable("id") Long id){
         return productService.findOne(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void removeOne(@PathVariable("id") Long id){
+        productService.removeOne(id);
     }
 
     @PutMapping
